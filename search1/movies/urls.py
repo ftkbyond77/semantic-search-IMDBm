35979a9d56c6188935c626b7d49b9e9a0397902a
Chayut Meekhun <chayutmeekhun4@gmail.com>
2025-06-08 15:17:10 +0700

    urls changed

--- search1/movies/urls.py
+++ search1/movies/urls.py
@@ -1,18 +1,21 @@
 from django.urls import path
 from . import views
+from django.shortcuts import redirect
 
 app_name = 'movies'
 
 urlpatterns = [
+    # Redirect root URL to login
+    path('', lambda request: redirect('movies:login'), name='root_redirect'),
+
     # Authentication routes
     path('login/', views.login_view, name='login'),
-    path('signup/', views.signup_view, name='signup'),  
+    path('signup/', views.signup_view, name='signup'),
     path('logout/', views.logout_view, name='logout'),
 
-    path('', views.home, name='home'),
+    # Main app routes
+    path('home/', views.home, name='home'),
     path('movie/<int:movie_id>/', views.movie_detail, name='movie_detail'),
-
-    # Admin-only routes
     path('export-search-data/', views.export_search_data, name='export_search_data'),
     path('search-analytics/', views.search_analytics, name='search_analytics'),
 ]
